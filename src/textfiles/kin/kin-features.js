1e23efb10134da6a9db6e0679b7a86f380cdaec8
dokdek <dokdek@gmail.com>
2021-01-15 00:29:14 +0000

    Edited text

--- src/textfiles/kin/kin-features.js
+++ src/textfiles/kin/kin-features.js
@@ -1,3 +1,3 @@
-const text = "The site is able to auth and connect to an Express backend via a custom REST API and jwt tokens. Once logged in, it queries the database for the logged in user's specific information and builds the UI based on that, displaying all their accounts, transactions and budget. Users are able to add, delete and update their budget, categories, and payment accounts."
+const text = "The site is able to authenticate users and connect to a MongoDB database via a REST API built on Node/Express. Once logged in, it queries the database for the logged in user's specific information and builds the UI based on that, displaying all their accounts, transactions and budget. Users are able to create custom categories and payment accounts, as well as add, delete, and update their budget."
 
 export default text;
