3ee15c5b2c436629f189e24b2b4257c86adeeb76
dokdek <dokdek@gmail.com>
2021-01-25 02:50:22 +0000

    Added blog posts for Shiroyuki

--- src/textfiles/shiroyuki/shiroyuki-blog.js
+++ src/textfiles/shiroyuki/shiroyuki-blog.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const text = [
   {
-    date: "",
-    content: "None."
+    date: "01/24/2021",
+    content: "After finishing the main features of the bot, I ran into a problem that pretty much renders the bot a bit useless. With the way I wanted the bot to DM the user the list of guilds that matches their search criteria, the bot would very quickly hit the Discord API rate limit which is in place to prevent spam. So the bot would not work in a true public setting as it would very easily hit the limits with real users."
+  },
+  {
+    date: "01/20/2021",
+    content: "Since the API requires a lot of async calls and promises, I ran into a classic problem of having too many chained promises and the code ends up being a triangle of callbacks which is extremely difficult to read. In future projects with multiple nested promises, I would try and extract them each into their own functions and call them sequentially, making the code a lot more readable."
   }
 ];
 
