c0c89edfc29c5326b53e3b7f345e9f3c4581d7df
dokdek <dokdek@gmail.com>
2021-03-01 18:56:58 -0500

    Added more info on projects

--- src/textfiles/personal/personal-features.js
+++ src/textfiles/personal/personal-features.js
@@ -1,3 +1,3 @@
-const text = "Able to easily push changes to github and have them reflected quickly. Able to easily add new pages/blog posts as most code is reusable and flexible."
+const text = "Able to easily push changes to Github and have them reflected quickly. Able to easily add new pages/blog posts as most code is reusable and flexible."
 
 export default text;
